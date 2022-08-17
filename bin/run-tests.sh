fd814cbf14c0af9571ab37c3eac6e1714cde400c
Petr Bílek <info@petrbilek.cz>
2022-08-17 13:01:48 +0200

    Add linux run scripts

---
+++ bin/run-tests.sh
@@ -0,0 +1,3 @@
+docker build -t integration-tmp .
+docker build -t integration-tmp-tests .\vendor\simplia\integration\docker\phpunit\
+docker run --rm -it integration-tmp-tests phpunit tests --bootstrap vendor/autoload.php
