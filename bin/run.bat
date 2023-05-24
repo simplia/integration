c9f9dc5676d22c1f639e6a29dc8fce7bd7c28c8e
Petr Soukup <soukup@simplia.cz>
2023-05-24 16:17:15 +0200

    fix local handler path

--- bin/run.bat
+++ bin/run.bat
@@ -1,2 +1,2 @@
 docker build -t integration-tmp .
-docker run --rm -it --env-file=.env --entrypoint= integration-tmp vendor/bin/bref-local handler=vendor/simplia/integration/handler.php %1
+docker run --rm -it --env-file=.env --entrypoint= integration-tmp vendor/bin/bref-local vendor/simplia/integration/handler.php %1
